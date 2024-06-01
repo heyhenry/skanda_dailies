1735b955509c37da58ee9aa69a3773dc94177f8a
heyhenry <henryyy3@hotmail.com>
2024-06-01 23:39:26 +0800

    Implemented money dailies

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ window.title("Skanda Dailies")
 
 
 content = tk.Frame(window).grid(row=0, column=0)
-player_name_lbl = tk.Label(content, text="Player Name: GreatHero", padx=10, pady=5).grid(row=0, column=0)
+player_name_lbl = tk.Label(content, text="Player Name: GreatHero", padx=10, pady=5).grid(row=0, column=0, columnspan=2)
+
 arcane_lbl = tk.Label(content, text="Daily Type: Arcane River Dailies", padx=10, pady=5).grid(row=1, column=0)
 vj_cb = tk.Checkbutton(content, text="Vanishing Journey", offvalue=0, onvalue=1).grid(row=2, column=0)
 chuchu_cb = tk.Checkbutton(content, text="Chu Chu Island", offvalue=0, onvalue=1).grid(row=3, column=0)
@@ -13,6 +14,10 @@ lach_cb = tk.Checkbutton(content, text="Lachelain", offvalue=0, onvalue=1).grid(
 arcana_cb = tk.Checkbutton(content, text="Arcana", offvalue=0, onvalue=1).grid(row=5, column=0)
 morass_cb = tk.Checkbutton(content, text="Morass", offvalue=0, onvalue=1).grid(row=6, column=0)
 esfera_cb = tk.Checkbutton(content, text="Esfera", offvalue=0, onvalue=1).grid(row=7, column=0)
+
+money_lbl = tk.Label(content, text="Daily Type: Money Dailies", padx=10, pady=5).grid(row=1, column=1)
+ursus_cb = tk.Checkbutton(content, text="Ursus", offvalue=0, onvalue=1).grid(row=2, column=1)
+mt_cb = tk.Checkbutton(content, text="Maple Tour", offvalue=0, onvalue=1).grid(row=3, column=1)
 
 
 window.mainloop()
