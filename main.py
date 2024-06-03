4975d138bd85292de51f161a8057e4ab4aa6076d
heyhenry <henryyy3@hotmail.com>
2024-06-04 00:05:09 +0800

    Got the add_ursus function working as per only when checked.

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ content = tk.Frame(window).grid(row=0, column=0)
 money_accum = 0
 bal = tk.IntVar()
 
-def add_ursus(m : int):
+def add_ursus():
     global money_accum
-    money_accum += m
-    # bal.set(money_accum)
+    if bal.get() == 1:
+        money_accum += 99000000
+    
+
+def total_calc():
+    pass
 
 def check_bal():
     print(money_accum)
@@ -28,7 +32,14 @@ morass_cb = tk.Checkbutton(content, text="Morass", offvalue=0, onvalue=1).grid(r
 esfera_cb = tk.Checkbutton(content, text="Esfera", offvalue=0, onvalue=1).grid(row=7, column=0)
 
 money_lbl = tk.Label(content, text="Daily Type: Money Dailies", padx=10, pady=5).grid(row=1, column=1)
-ursus_cb = tk.Checkbutton(content, text="Ursus", variable=bal,).grid(row=2, column=1)
+
+
+##
+ursus_cb = tk.Checkbutton(content, text="Ursus", variable=bal, command=add_ursus).grid(row=2, column=1)
+##
+
+
+
 mt_cb = tk.Checkbutton(content, text="Maple Tour", offvalue=0, onvalue=1).grid(row=3, column=1)
 
 bal_check = tk.Button(content, text='Check Bal', command=check_bal).grid(row=0, column=2)
