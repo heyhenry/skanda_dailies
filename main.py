390591856a50500dfe71e78dc9de51a527d79670
heyhenry <henryyy3@hotmail.com>
2024-06-03 23:58:40 +0800

    updated bal to intvar for default uncheck in ursus.

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ window.title("Skanda Dailies")
 content = tk.Frame(window).grid(row=0, column=0)
 
 money_accum = 0
-bal = tk.StringVar()
+bal = tk.IntVar()
 
 def add_ursus(m : int):
     global money_accum
     money_accum += m
-    bal.set(money_accum)
+    # bal.set(money_accum)
 
 def check_bal():
     print(money_accum)
+    # print(bal.get())
 
 player_name_lbl = tk.Label(content, text="Player Name: GreatHero", padx=10, pady=5).grid(row=0, column=0, columnspan=2)
 
@@ -27,7 +28,7 @@ morass_cb = tk.Checkbutton(content, text="Morass", offvalue=0, onvalue=1).grid(r
 esfera_cb = tk.Checkbutton(content, text="Esfera", offvalue=0, onvalue=1).grid(row=7, column=0)
 
 money_lbl = tk.Label(content, text="Daily Type: Money Dailies", padx=10, pady=5).grid(row=1, column=1)
-ursus_cb = tk.Checkbutton(content, text="Ursus", offvalue=0, onvalue=1, command=add_ursus(99000000)).grid(row=2, column=1)
+ursus_cb = tk.Checkbutton(content, text="Ursus", variable=bal,).grid(row=2, column=1)
 mt_cb = tk.Checkbutton(content, text="Maple Tour", offvalue=0, onvalue=1).grid(row=3, column=1)
 
 bal_check = tk.Button(content, text='Check Bal', command=check_bal).grid(row=0, column=2)
