6e560dc02616253c9fa6c8b5e52f595241089313
heyhenry <henryyy3@hotmail.com>
2024-06-09 09:57:37 +0800

    Updated to make result_label useful

--- samples/dropdown_sample.py
+++ samples/dropdown_sample.py
@@ -4,16 +4,18 @@ import tkinter as tk
 
 def on_option_select(event):
     selected_option.set(event)
+    display_size.set(event)
 
 root = tk.Tk()
 root.title("Dropdown Menu Example")
 root.geometry("400x300")
 
 # create a string var to hold the selected option
-selected_option = tk.StringVar()
+selected_option = tk.IntVar(value="Select Party Size")
+display_size = tk.IntVar()
 
 # create the dropdown menu
-options = ["Option 1", "Option 2", "Option 3", "Option 4"]
+options = [1, 2, 3, 4, 5, 6]
 dropdown = tk.OptionMenu(root, selected_option, *options)
 dropdown.pack(pady=10)
 
@@ -22,7 +24,7 @@ show_button = tk.Button(root, text="Show Selection", command=lambda: on_option_s
 show_button.pack()
 
 # label to display the selected option
-result_label = tk.Label(root, text="")
+result_label = tk.Label(root, textvariable=display_size)
 result_label.pack()
 
 root.mainloop()
