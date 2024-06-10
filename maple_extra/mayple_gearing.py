ed4e8a1428192370f80c1e9a710cc204fe8786ac
heyhenry <henryyy3@hotmail.com>
2024-06-10 11:35:16 +0800

    reposition starting window

--- maple_extra/mayple_gearing.py
+++ maple_extra/mayple_gearing.py
@@ -1,21 +1,11 @@
 import tkinter as tk
 
-
 root = tk.Tk()
 root.title('Mayple Gearing Checklist')
-root.geometry('400x300')
+root.geometry('550x500+700+200')
 
-char_name = tk.StringVar()
-display_name = tk.StringVar()
 
-def update_display():
-    name = char_name.get()
-    display_name.set(name)
 
-name_lbl = tk.Label(root, text='Character Name: ').pack()
-name_entry = tk.Entry(root, textvariable=char_name).pack()
-namesub_btn = tk.Button(root, text='submit', command=update_display).pack()
 
-display_lbl = tk.Label(root, textvariable=display_name).pack()
 
 root.mainloop()
