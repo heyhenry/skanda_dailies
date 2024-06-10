8d4b77ce9b5d061d05b54401fa37dca58f682c09
heyhenry <henryyy3@hotmail.com>
2024-06-10 12:39:55 +0800

    Implemented character counter

--- maple_extra/mayple_gearing.py
+++ maple_extra/mayple_gearing.py
@@ -18,26 +18,36 @@ notebook.add(frame1, text='Tab 1')
 notebook.add(frame2, text='Tab 2')
 
 # frame 1 (character information input / counter)
+counter = 0
 completed_counter = tk.IntVar()
 char_name = tk.StringVar()
 current_char = tk.StringVar()
 
 def display_name():
+    global counter
     curr_name = char_name.get()
     current_char.set(curr_name)
     char_name.set('')
+    counter += 1
+    completed_counter.set(counter)
 
 name_lbl = tk.Label(frame1, text="Enter Character Name: ")
 name_entry = tk.Entry(frame1, textvariable=char_name, justify='center')
-sub_btn = tk.Button(frame1, text="Submit", command=lambda: display_name())
-displayname_lbl = tk.Label(frame1, textvariable=current_char) 
+displayname_lbllbl = tk.Label(frame1, text="Current Character: ")
+displayname_lbl = tk.Label(frame1, textvariable=current_char)
+counter_lbllbl = tk.Label(frame1, text='Character Counter: ') 
+counter_lbl = tk.Label(frame1, textvariable=completed_counter)
+sub_btn = tk.Button(frame1, text="Submit", command=display_name)
 
 name_lbl.grid(row=0, column=0)
 name_entry.grid(row=0, column=1)
-sub_btn.grid(row=1, column=0)
-displayname_lbl.grid(row=2, column=0)
+displayname_lbllbl.grid(row=1, column=0)
+displayname_lbl.grid(row=1, column=1)
+counter_lbllbl.grid(row=2, column=0)
+counter_lbl.grid(row=2, column=1)
+sub_btn.grid(row=3, columnspan=2)
 
-test_lbl = tk.Label(frame2, text="TEST TEXT")
+test_lbl = tk.Label(frame2, textvariable=current_char)
 test_lbl.pack()
 
 
