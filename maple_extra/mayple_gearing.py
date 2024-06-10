de77bd6d945430f8cb0670b3797a42ad78f3a10f
heyhenry <henryyy3@hotmail.com>
2024-06-10 11:57:20 +0800

    Created tabbing

--- maple_extra/mayple_gearing.py
+++ maple_extra/mayple_gearing.py
@@ -1,11 +1,20 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
 root.title('Mayple Gearing Checklist')
 root.geometry('550x500+700+200')
 
+notebook = ttk.Notebook(root)
+notebook.pack(expand=True, fill='both')
 
+frame1 = tk.Frame(notebook, bg='#E6E6FA')
+frame2 = tk.Frame(notebook, bg='#F0F8FF')
 
+frame1.pack()
+frame2.pack()
 
+notebook.add(frame1, text='Tab 1')
+notebook.add(frame2, text='Tab 2')
 
 root.mainloop()
