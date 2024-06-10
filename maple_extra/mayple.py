64ef68c9ba7b357d47785b2c5ba77af59a61258a
heyhenry <henryyy3@hotmail.com>
2024-06-10 15:58:19 +0800

    initial template for mayple (formerly known as mayple gearing)

---
+++ maple_extra/mayple.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+
+# window setup
+root = tk.Tk()
+root.title('Mayple Helper')
+root.geometry('400x300')
+
+# functions
+
+# variables
+
+# cdt_frame (character details tab)
+
+# clt_frame (character list tab)
+
+# sat_frame (satistical analysis tab)
+
+
+root.mainloop()
