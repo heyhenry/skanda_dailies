d2b2a7aed716dc73d5ae5754189fd2d3ef684442
heyhenry <henryyy3@hotmail.com>
2024-06-11 11:36:25 +0800

    customising json serialization

--- maple_extra/json_play.py
+++ maple_extra/json_play.py
@@ -10,17 +10,23 @@ obj1 = CharInfo('Henry', 'Mage', 21)
 obj2 = CharInfo('Jeo', 'Warrior', 22)
 obj3 = CharInfo('Mas', 'knight', 22)
 
-chars = []
-chars.append(obj1)
-chars.append(obj2)
-chars.append(obj3)
+chars = [obj1, obj2, obj3]
 
 chars_dict = {}
 
-for player in chars:
-    chars_dict[player.name] = [player.name, player.role, player.level]
+for char in chars:
+    chars_dict[char.name] = char
 
-json_data = json.dumps(chars_dict, indent=4)
+def custom_json_serializer(obj):
+    if isinstance(obj, CharInfo):
+        return {
+            'name': obj.name,
+            'role': obj.role,
+            'level': obj.level
+        }
+    return obj
+
+json_data = json.dumps(chars_dict, default=custom_json_serializer, indent=4)
 
 with open('sample.json', 'w') as outfile:
     outfile.write(json_data)
