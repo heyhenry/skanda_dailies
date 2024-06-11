43a5bec8b56700add2e7f14997cc8387a99334cc
heyhenry <henryyy3@hotmail.com>
2024-06-11 12:53:41 +0800

    added comments for clarity/readability

--- maple_extra/json_based/json_second_play.py
+++ maple_extra/json_based/json_second_play.py
@@ -1,20 +1,25 @@
 import tkinter as tk
 import json
 
+# json file storage variable
 json_filename = 'jsoning.json'
 
+# object class for character
 class CharInfo:
     def __init__(self, name, race, level):
         self.name = name
         self.race = race
         self.level = level
 
+# tkinter window setup
 root = tk.Tk()
 root.title('Jsoning')
 root.geometry('300x300')
 
+# dictionary to house all the character objects
 characters = {}
 
+# custom serializer to intepret and process data to json file the way i want it to
 def json_serializer(obj):
     if isinstance(obj, CharInfo):
         return {
@@ -24,6 +29,8 @@ def json_serializer(obj):
         }
     return obj
 
+# save character submission function
+# stores the characters in the same-name dictionary as well as in the json save file
 def save_characters():
     char_name = name_entry.get()
     char_race = race_entry.get()
@@ -36,11 +43,13 @@ def save_characters():
     with open(json_filename, 'w') as outfile:
         outfile.write(json_data)
 
+# print/display character details
+# pulls information from the characters dictionary
 def print_characters():
     for char_name, char_info in characters.items():
         print(f"Name: {char_name}, Race: {char_info.race}, Level: {char_info.level}")
 
-
+# tkinter widgets for character input
 name_entry = tk.Entry(root)
 name_entry.insert(0, 'Enter Name')
 
@@ -58,6 +67,5 @@ race_entry.pack()
 level_entry.pack()
 sub_btn.pack()
 print_btn.pack()
-
 
 root.mainloop()
