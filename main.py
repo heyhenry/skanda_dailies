91a2d0feedb39e96ab671421e68d077a95ec2782
heyhenry <henryyy3@hotmail.com>
2024-06-28 00:18:20 +0800

    implemented print character detail function

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import json
 import os
 from tkinter import ttk
 
+
+
+def print_details():
+    char_name = charadd_name.get()
+    char_class = charadd_class.get()
+    char_level = charadd_level.get()
+
+    print(f"Name: {char_name} | Class: {char_class} | Level: {char_level}")
+
+
 root = tk.Tk()
 root.title('Skanda')
 root.geometry('300x300')
@@ -38,6 +48,8 @@ charadd_class = tk.Entry(charadd_frame)
 charadd_level_lbl = tk.Label(charadd_frame, text='Character Level:')
 charadd_level = tk.Entry(charadd_frame)
 
+charadd_btn = tk.Button(charadd_frame, text='Submit', command=print_details)
+
 charadd_title.grid(row=0, columnspan=2)
 charadd_name_lbl.grid(row=1, column=0)
 charadd_class_lbl.grid(row=2, column=0)
@@ -45,6 +57,7 @@ charadd_level_lbl.grid(row=3, column=0)
 charadd_name.grid(row=1, column=1)
 charadd_class.grid(row=2, column=1)
 charadd_level.grid(row=3, column=1)
+charadd_btn.grid(row=4, columnspan=2)
 
 # checklist frame
 
